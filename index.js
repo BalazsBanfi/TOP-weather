07ebf12dca17e10c890dc0799701cf942b2062da
BalazsBanfi <banfibalazs81@gmail.com>
2023-11-06 11:46:30 +0100

    Add render page

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 import { api } from "./env.js";
+const search = document.getElementById('search')
 
-console.log(api);
-const city = "London"
+const city = document.getElementById('city');
+const temp = document.getElementById('temp');
+const feels = document.getElementById('feels');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
 
-//const img = document.querySelector('img');
-fetch(`${api.url}current.json?key=${api.key}&q=${city}`, { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response);
-    });
+const render = (data) => {
+    console.log(data);
+    city.innerHTML = `${data.location.name}, ${data.location.country}`;
+    temp.innerHTML = `Temperature: ${data.current.temp_c} Celsius`;
+    feels.innerHTML = `Feels like ${data.current.feelslike_c} Celsius`;
+    humidity.innerHTML = `Humidity: ${data.current.humidity} %`;
+    wind.innerHTML = `Wind: ${data.current.wind_kph} km/h`;
+}
+
+async function getCity(loc) {
+    fetch(`${api.url}current.json?key=${api.key}&q=${loc}`, { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            render(response);
+        });
+}
+
+
+
+search.addEventListener('keypress', (e) => {
+    console.log(e)
+    console.log(search.innerHTML)
+    
+    if (e.key == 'Enter') {
+        e.preventDefault;
+        getCity(search.innerHTML)
+    }
+})
+
+getCity('London');
